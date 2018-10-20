b262fbdc9517e9c5d73852e2fda2f5a5b695e964
Andrew Collinson <ffloyd2000@gmail.com>
2018-10-20 19:19:10 +0100

    Added Captcha to form.

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,32 +1,15 @@
 import React, { Component } from 'react';
-import './form.js';
+import ReCAPTCHA from "react-google-recaptcha";
 import './ContactForm.css';
 
 class ContactForm extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleSubmit(event) {
-    const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    console.log('errorred');
-    const msg = {
-      to: 'ffloyd2000@gmail.com',
-      from: 'info@muddy-pups.co.uk',
-      subject: 'Sending with SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
 
-    sgMail.send(msg);
-  }
   render() {
     return (
       <section className="ContactForm" id="contact">
         <div className="inner">
           <h2>Contact us</h2>
-          <form onSubmit={this.handleSubmit}>
+          <form method="post">
             <fieldset>
               <legend>To contact us please fill in the form below:</legend>
               <div className="formRow">
@@ -67,6 +50,15 @@ class ContactForm extends Component {
                 </div>
                 <div className="inputCol">
                   <input type="file" name="yourDog" id="yourDog" placeholder="Your dog" />
+                </div>
+              </div>
+              <div className="formRow">
+                <div className="labelCol">
+                </div>
+                <div className="inputCol">
+                  <ReCAPTCHA
+                    sitekey="6LeV7HUUAAAAABgFBZl8lEv3v_AcxihUvTatIBW-"
+                  />
                 </div>
               </div>
               <div className="buttonRow">
